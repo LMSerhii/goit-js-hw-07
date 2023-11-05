5472dcf00fe71ac226a7fb6e3cbbd81f75b89c08
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-11-05 16:10:34 +0200

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryElement = document.querySelector(".gallery");
+
+galleryElement.insertAdjacentHTML("beforeend", insertGalleryHTML(galleryItems));
+
+galleryElement.addEventListener("click", onGalleryElement);
+
+function insertGalleryHTML(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                <img src="${preview}" alt="${description}" data-original="${original}" class="gallery__image" />
+            </li>`;
+    })
+    .join("");
+}
+
+function onGalleryElement(event) {
+  const originalUrl = event.target.dataset.original;
+  const isGalleryImageEl = event.target.classList.contains("gallery__image");
+
+  if (!isGalleryImageEl) {
+    return;
+  }
+
+  console.log(originalUrl);
+
+  const instance = basicLightbox.create(
+    `<img src="${originalUrl}" width="1200">`
+  );
+
+  instance.show();
+}
