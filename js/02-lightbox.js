b8ad6c20f046246c73d23943028052ad7f30e1b3
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-11-07 22:23:02 +0200

    Update task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,13 +1,10 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
-
 const galleryElement = document.querySelector(".gallery");
 const galleryMurkup = insertGalleryHTML(galleryItems);
 
 galleryElement.insertAdjacentHTML("beforeend", galleryMurkup);
-galleryElement.addEventListener("click", onGalleryElement);
 
 function insertGalleryHTML(galleryItems) {
   return galleryItems
@@ -21,25 +18,13 @@ function insertGalleryHTML(galleryItems) {
     .join("");
 }
 
-function onGalleryElement(event) {
-  event.preventDefault();
+const options = {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  enableKeyboard: true,
+  docClose: true,
+  doubleTapZoom: 2,
+};
 
-  const isGalleryImageEl = event.target.classList.contains("gallery__image");
-
-  if (!isGalleryImageEl) {
-    return;
-  }
-
-  const options = {
-    captionsData: "alt",
-    captionPosition: "bottom",
-    captionDelay: 250,
-    enableKeyboard: true,
-    docClose: true,
-    doubleTapZoom: 2,
-  };
-
-  const lightbox = new SimpleLightbox(".gallery a", options);
-
-  lightbox.on("error.simplelightbox", (e) => console.log(e));
-}
+const lightbox = new SimpleLightbox(".gallery a", options);
